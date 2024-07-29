d8b13a7df9e1cdd0a1a505302d07dcb2f24fcffe
Natalia B <naffa.bob@gmail.com>
2024-07-29 18:09:28 +0300

    Добавлены модели для API

---
+++ fastapi_movies/src/api/v1/api_models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
+
+
+class IdMixIn(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+
+
+class Genre(IdMixIn):
+    name: str
+
+
+class Person(IdMixIn):
+    full_name: str = Field(alias='name')
+
+
+class FilmDetails(IdMixIn):
+    title: str
+    imdb_rating: float
+    description: str | None
+    genre: list[Genre] = Field(alias="genres")
+    actors: list[Person]
+    writers: list[Person]
+    directors: list[Person]
