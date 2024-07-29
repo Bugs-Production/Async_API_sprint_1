878795f2f145cbe88ee68e97be084bfe41e181b3
Natalia B <naffa.bob@gmail.com>
2024-07-29 18:08:13 +0300

    Добавлены модели для Elastic, Redis

--- fastapi_movies/src/models/film.py
+++ fastapi_movies/src/models/film.py
@@ -1,8 +1,28 @@
 # Используем pydantic для упрощения работы при перегонке данных из json в объекты
+# В этом файле описаны модели бизнес-логики
 from pydantic import BaseModel
 
 
-class Film(BaseModel):
+class IdMixIn(BaseModel):
     id: str
+
+
+class Genre(IdMixIn):
+    name: str
+
+
+class Person(IdMixIn):
+    name: str
+
+
+class Film(IdMixIn):
     title: str
-    description: str
+    description: str | None
+    imdb_rating: float | None
+    genres: list[Genre]
+    actors: list[Person] | None
+    writers: list[Person] | None
+    directors: list[Person] | None
+    actors_names: list[str] | None
+    writers_names: list[str] | None
+    directors_names: list[str] | None
