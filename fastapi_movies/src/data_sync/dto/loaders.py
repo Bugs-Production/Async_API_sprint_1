a34731df77a5fdffea82bd596f5ba67494e1ca50
vtitoff <v.titoff2010@gmail.com>
2024-07-29 22:00:03 +0300

    Вынести процесс загрузки в эластик в отдельный класс LoadManager

--- fastapi_movies/src/data_sync/dto/loaders.py
+++ fastapi_movies/src/data_sync/dto/loaders.py
@@ -1,28 +1,47 @@
 import logging
 from abc import ABC, abstractmethod
-from typing import List
+from datetime import datetime
+from typing import Any, Dict, List
 
+import psycopg
 from elasticsearch import Elasticsearch
 from pydantic import BaseModel
 
 from data_sync.dto.models import ElasticFilmWork, PostgresFilmWork
+from data_sync.state.state import State
+from data_sync.utils.constants import PG_FETCH_SIZE
+from data_sync.utils.decorators import backoff
 from data_sync.utils.utils import create_elastic_objects_list
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+class Postgres:
+    def __init__(self, conn: psycopg.Connection):
+        self.conn = conn
+
+    @backoff()
+    def execute(self, sql_path, params: Dict[str, Any]) -> List[dict]:
+        with self.conn.cursor() as cursor:
+            with open(sql_path, "rb") as f:
+                query = f.read()
+            cursor.execute(query, params)
+            rows = cursor.fetchmany(PG_FETCH_SIZE)
+            return rows
+
+
 class PostgresExtractor(ABC):
     @staticmethod
     @abstractmethod
-    def extract_films(data: dict):
+    def extract(data: dict):
         """Валидирует данные, пришедшие из Postgres."""
         pass
 
 
 class FilmsPostgresExtractor(PostgresExtractor):
     @staticmethod
-    def extract_films(data: dict) -> PostgresFilmWork:
+    def extract(data: dict) -> PostgresFilmWork:
         film_work = PostgresFilmWork(
             id=data["id"],
             title=data["title"],
@@ -94,3 +113,44 @@ class ElasticLoader:
             body.append({**_object.model_dump()})
         res = client.bulk(index=index, body=body)
         logger.info(res)
+
+
+class Task:
+    def __init__(
+        self,
+        state_key: str,
+        elastic_index: str,
+        extractor: PostgresExtractor,
+        el_transformer: ElasticTransformer,
+        sql_path: str,
+    ):
+        self.state_key = state_key
+        self.elastic_index = elastic_index
+        self.extractor = extractor
+        self.el_transformer = el_transformer
+        self.sql_path = sql_path
+
+
+class LoadManager:
+    def __init__(self, pg: Postgres, elastic: Elasticsearch, state: State):
+        self.pg = pg
+        self.elastic = elastic
+        self.state = state
+
+    def load_to_elastic(self, task: Task):
+        last_modified_obj = self.state.get_state(task.state_key, datetime.min)
+        while pg_data := self.pg.execute(
+            sql_path=task.sql_path, params={"dttm": last_modified_obj}
+        ):
+            elastic_objects = []
+            tmp_last_obj_modified = last_modified_obj
+            for obj in pg_data:
+                pg_obj = task.extractor.extract(obj)
+                elastic_obj = task.el_transformer.transform(pg_obj)
+                elastic_objects.append(elastic_obj)
+                tmp_last_obj_modified = pg_obj.modified
+            ElasticLoader.load(
+                self.elastic, task.elastic_index, elastic_objects
+            )
+            last_modified_obj = tmp_last_obj_modified
+            self.state.save_state(task.state_key, str(last_modified_obj))
--- fastapi_movies/src/data_sync/postgres_to_elastic.py
+++ fastapi_movies/src/data_sync/postgres_to_elastic.py
@@ -1,6 +1,4 @@
 import logging
-from datetime import datetime
-from typing import Any, Dict, List
 
 import psycopg
 from elasticsearch import Elasticsearch, NotFoundError
@@ -8,58 +6,24 @@ from psycopg import ClientCursor
 from psycopg.rows import dict_row
 
 from data_sync.config.config import ElasticSettings, PostgresSettings
-from data_sync.utils.constants import (ETL_FILMS_MAPPING, FILM_WORK_STATE_KEY,
-                                       GENRE_STATE_KEY, MOVIES_INDEX,
-                                       PG_FETCH_SIZE)
+from data_sync.utils.constants import (
+    ETL_FILMS_MAPPING,
+    FILM_WORK_STATE_KEY,
+    MOVIES_INDEX,
+)
 from data_sync.utils.decorators import backoff
-from dto.loaders import (ElasticLoader, FilmsElasticTransformer,
-                         FilmsPostgresExtractor)
+from dto.loaders import (
+    FilmsElasticTransformer,
+    FilmsPostgresExtractor,
+    LoadManager,
+    Postgres,
+    Task,
+)
 from state.json_storage import JsonStorage
 from state.state import State
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-class Postgres:
-    def __init__(self, conn: psycopg.Connection, sql_path: str):
-        self.conn = conn
-        self.query = self.get_query(sql_path)
-
-    def get_query(self, sql_path: str) -> bytes:
-        with open(sql_path, "rb") as f:
-            self.query = f.read()
-            return self.query
-
-    @backoff()
-    def execute(self, params: Dict[str, Any]) -> List[dict]:
-        with self.conn.cursor() as cursor:
-            cursor.execute(self.query, params)
-            rows = cursor.fetchmany(PG_FETCH_SIZE)
-            return rows
-
-
-def load_films(
-        pg_conn: psycopg.Connection,
-        state: State,
-        elastic: Elasticsearch
-):
-    pg = Postgres(
-        conn=pg_conn,
-        sql_path="storage/postgresql/queries/load_films.sql"
-    )
-    last_modified_film = state.get_state(FILM_WORK_STATE_KEY, datetime.min)
-    while film_works_data := pg.execute(params={"dttm": last_modified_film}):
-        elastic_films = []
-        tmp_last_film_modified = last_modified_film
-        for fm in film_works_data:
-            pg_film_work = FilmsPostgresExtractor.extract_films(fm)
-            elastic_film_work = FilmsElasticTransformer.transform(pg_film_work)
-            elastic_films.append(elastic_film_work)
-            tmp_last_film_modified = pg_film_work.modified
-        ElasticLoader.load(elastic, MOVIES_INDEX, elastic_films)
-        last_modified_film = tmp_last_film_modified
-        state.save_state(FILM_WORK_STATE_KEY, str(last_modified_film))
 
 
 @backoff()
@@ -86,7 +50,16 @@ def main():
     with psycopg.connect(
         **dsl, row_factory=dict_row, cursor_factory=ClientCursor
     ) as pg_conn:
-        load_films(pg_conn, state, elastic)
+        pg = Postgres(pg_conn)
+        manager = LoadManager(pg=pg, elastic=elastic, state=state)
+        film_work_task = Task(
+            state_key=FILM_WORK_STATE_KEY,
+            elastic_index=MOVIES_INDEX,
+            extractor=FilmsPostgresExtractor,
+            el_transformer=FilmsElasticTransformer,
+            sql_path="storage/postgresql/queries/load_films.sql",
+        )
+        manager.load_to_elastic(film_work_task)
 
 
 if __name__ == "__main__":
