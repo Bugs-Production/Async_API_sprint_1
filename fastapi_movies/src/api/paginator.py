cdea0e8d16ae9e2b33c84f96c006c5f2fd4488a6
Natalia B <naffa.bob@gmail.com>
2024-08-01 00:57:40 +0300

    Добавлена валидация для page_number, page_size

--- fastapi_movies/src/api/paginator.py
+++ fastapi_movies/src/api/paginator.py
@@ -1,6 +1,8 @@
+from fastapi import Query
 from pydantic import BaseModel
+from typing import Annotated
 
 
 class Paginator(BaseModel):
-    page_number: int = 1
-    page_size: int = 50
+    page_number: Annotated[int, Query(default=1, gt=0)]
+    page_size: Annotated[int, Query(default=50, gt=0)]
