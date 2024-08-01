38acf100cc9b4e8409d653c6e98506fee2cf7ac1
vtitoff <v.titoff2010@gmail.com>
2024-08-01 09:59:05 +0300

    Поправить разделители

--- fastapi_movies/src/data_sync/storage/postgresql/queries/load_persons.sql
+++ fastapi_movies/src/data_sync/storage/postgresql/queries/load_persons.sql
@@ -1,1 +1,36 @@
-SELECT   uuid,   full_name,   modified,   COALESCE (json_agg(DISTINCT jsonb_build_object(      'id', film_work_id,      'roles', roles,      'title', title,      'rating', rating   )), '[]') as "films"FROM (    SELECT        p.uuid,        p.full_name,        p.modified,        pf.film_work_id,        fw.title,        fw.rating,        ARRAY_AGG(pf.role) as roles    FROM (        SELECT            p.id uuid,            p.full_name,            p.created,            p.modified        FROM content.person p        WHERE p.modified > %(dttm)s        LIMIT 100    ) p    JOIN content.person_film_work pf    ON p.uuid=pf.person_id    JOIN content.film_work fw    ON pf.film_work_id=fw.id    GROUP BY p.uuid, p.full_name, pf.film_work_id, fw.title, fw.rating, p.modified)GROUP BY uuid, full_name, modified
+SELECT
+   uuid,
+   full_name,
+   modified,
+   COALESCE (json_agg(DISTINCT jsonb_build_object(
+      'id', film_work_id,
+      'roles', roles,
+      'title', title,
+      'rating', rating
+   )), '[]') as "films"
+FROM (
+    SELECT
+        p.uuid,
+        p.full_name,
+        p.modified,
+        pf.film_work_id,
+        fw.title,
+        fw.rating,
+        ARRAY_AGG(pf.role) as roles
+    FROM (
+        SELECT
+            p.id uuid,
+            p.full_name,
+            p.created,
+            p.modified
+        FROM content.person p
+        WHERE p.modified > %(dttm)s
+        LIMIT 100
+    ) p
+    JOIN content.person_film_work pf
+    ON p.uuid=pf.person_id
+    JOIN content.film_work fw
+    ON pf.film_work_id=fw.id
+    GROUP BY p.uuid, p.full_name, pf.film_work_id, fw.title, fw.rating, p.modified
+)
+GROUP BY uuid, full_name, modified
