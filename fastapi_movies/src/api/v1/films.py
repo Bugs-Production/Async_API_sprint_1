fc26a9fdaf7c89aa8c4468520a8d88b62e3a1854
Natalia B <naffa.bob@gmail.com>
2024-08-02 00:54:04 +0300

    API модели: переименованы, добавлены модели для персон

--- fastapi_movies/src/api/v1/films.py
+++ fastapi_movies/src/api/v1/films.py
@@ -3,19 +3,19 @@ from http import HTTPStatus
 from fastapi import APIRouter, Depends, HTTPException
 
 from api.paginator import Paginator
-from .api_models import FilmDetails, FilmShort
+from .api_models import Film, FilmDetail
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
 
 
-@router.get('/search', response_model=list[FilmShort])
+@router.get('/search', response_model=list[Film])
 async def film_search(
     query: str,
     sort: str = '-imdb_rating',
     paginator: Paginator = Depends(Paginator),
     film_service: FilmService = Depends(get_film_service)
-) -> list[FilmShort]:
+) -> list[Film]:
 
     searched_films = await film_service.search_films(
         query=query,
@@ -27,16 +27,16 @@ async def film_search(
     if not searched_films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
 
-    return [FilmShort(**film.dict()) for film in searched_films]
+    return [Film(**film.dict()) for film in searched_films]
 
 
-@router.get("/", response_model=list[FilmShort])
+@router.get("/", response_model=list[Film])
 async def films(
     sort: str | None = "-imdb_rating",
     genre: str | None = None,
     paginator: Paginator = Depends(Paginator),
     film_service: FilmService = Depends(get_film_service)
-) -> list[FilmShort]:
+) -> list[Film]:
     """
     Для сортировки используется default="-imdb_rating" по бизнес логике,
     чтобы всегда выводились только популярные фильмы
@@ -51,17 +51,17 @@ async def films(
     if not all_films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
 
-    return [FilmShort(**film.dict()) for film in all_films]
+    return [Film(**film.dict()) for film in all_films]
 
 
-@router.get("/{film_id}", response_model=FilmDetails)
+@router.get("/{film_id}", response_model=FilmDetail)
 async def film_details(
     film_id: str, film_service: FilmService = Depends(get_film_service)
-) -> FilmDetails:
+) -> FilmDetail:
     film = await film_service.get_by_id(film_id)
 
     if not film:
         # Если фильм не найден, отдаём 404 статус
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
 
-    return FilmDetails(**film.dict())
+    return FilmDetail(**film.dict())
