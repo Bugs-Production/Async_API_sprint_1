f689b31a23f737d8d6340f886022ab9ebaab0f98
vtitoff <v.titoff2010@gmail.com>
2024-08-03 18:12:40 +0300

    Заменить response model на GenreDetail

--- fastapi_movies/src/api/v1/genres.py
+++ fastapi_movies/src/api/v1/genres.py
@@ -1,27 +1,28 @@
 from http import HTTPStatus
 
 from fastapi import APIRouter, Depends, HTTPException
+
 from api.paginator import Paginator
-from .api_models import Genre, GenreDetail
 from services.genre import GenreService, get_genre_service
 
+from .api_models import GenreDetail
 
 router = APIRouter()
 
-@router.get("/", response_model=list[Genre])
+
+@router.get("/", response_model=list[GenreDetail])
 async def genres(
-        paginator: Paginator = Depends(Paginator),
-        genre_service: GenreService = Depends(get_genre_service)
-) -> list[Genre]:
+    paginator: Paginator = Depends(Paginator),
+    genre_service: GenreService = Depends(get_genre_service),
+) -> list[GenreDetail]:
     all_genres = await genre_service.get_all_genres(
         page_num=paginator.page_number,
         page_size=paginator.page_size,
     )
     if not all_genres:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
-                            detail="genres not found")
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="genres not found")
 
-    return [Genre(**genre.model_dump()) for genre in all_genres]
+    return [GenreDetail(**genre.model_dump()) for genre in all_genres]
 
 
 @router.get("/{genre_id}", response_model=GenreDetail)
@@ -33,4 +34,4 @@ async def genre_details(
     if not genre:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="genre not found")
 
-    return GenreDetail(**genre.dict())
+    return GenreDetail(**genre.model_dump())
