0165bc9ba068af44e9eac879ad755cb04290d079
vtitoff <v.titoff2010@yandex.ru>
2024-08-13 00:30:05 +0300

    Вынести соединения в отдельные фикстуры

--- fastapi_movies/src/tests/functional/conftest.py
+++ fastapi_movies/src/tests/functional/conftest.py
@@ -8,17 +8,23 @@ from elasticsearch.helpers import async_bulk
 from tests.functional.settings import test_settings
 
 
-@pytest.fixture(name="es_write_data")
-def es_write_data():
+@pytest.fixture(scope="session")
+async def es_client():
+    es_client = AsyncElasticsearch(hosts=test_settings.es_host, verify_certs=False)
+    yield es_client
+    await es_client.close()
+
+
+@pytest.fixture(scope="session")
+def es_write_data(es_client: AsyncElasticsearch):
     async def inner(data: list[dict], index: str, mapping: dict):
-        es_client = AsyncElasticsearch(hosts=test_settings.es_host, verify_certs=False)
         if await es_client.indices.exists(index=index):
             await es_client.indices.delete(index=index)
         await es_client.indices.create(index=index, **mapping)
 
-        updated, errors = await async_bulk(client=es_client, actions=data)
-
-        await es_client.close()
+        updated, errors = await async_bulk(
+            client=es_client, actions=data, refresh="wait_for"
+        )
 
         if errors:
             raise Exception("Ошибка записи данных в Elasticsearch")
@@ -26,16 +32,24 @@ def es_write_data():
     return inner
 
 
-@pytest.fixture(name="aiohttp_client_data")
-def aiohttp_client_data():
+@pytest.fixture(scope="session")
+async def aiohttp_session():
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest.fixture(scope="session")
+def aiohttp_client_data(aiohttp_session: aiohttp.ClientSession):
     async def inner(
         method: str, endpoint: str, **kwargs: dict[str, Any]
     ) -> tuple[dict, int]:
-        async with aiohttp.ClientSession() as session:
-            url = test_settings.service_url + endpoint
-            async with session.request(method=method, url=url, **kwargs) as response:
-                body = await response.json()
-                status = response.status
+        url = test_settings.service_url + endpoint
+        async with aiohttp_session.request(
+            method=method, url=url, **kwargs
+        ) as response:
+            body = await response.json()
+            status = response.status
             return body, status
 
     return inner
